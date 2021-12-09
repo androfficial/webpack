5436e7ba7b6426800ef04a6a07aea768ab3471d4
androfficial <material1fox@gmail.com>
2021-12-09 20:34:42 +0200

    Changes: - Added health check HMR (saving component state) - Styles file improvements

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,10 +1,29 @@
+import { useCallback, useState } from 'react';
 import Footer from '../Footer';
 
-const App = () => (
-  <div className='webpack'>
-    <strong>WEBPACK 5</strong>
-    <Footer />
-  </div>
-);
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseCounter = useCallback(() => {
+    setCounter((prev) => prev + 1);
+  }, [setCounter]);
+
+  return (
+    <div className='webpack'>
+      <strong>WEBPACK 5</strong>
+      <Footer />
+      <div className='webpack__info'>
+        <p className='webpack__text'>Example Hot Module Replacement</p>
+        <button
+          onClick={increaseCounter}
+          className='webpack__button'
+          type='button'
+        >
+          Counter: <span>{counter}</span>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
